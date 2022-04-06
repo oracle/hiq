73c40afd0ce93ad4c76ec84a0c5ee4500473a0da
Fuheng Wu <fuheng.wu@oracle.com>
2022-04-05 23:31:33 -0700

    paddle

--- hiq/examples/paddle/main_driver2.py
+++ hiq/examples/paddle/main_driver2.py
@@ -1,38 +1,22 @@
+import os
 import hiq
-from paddleocr import *
-import paddle
-import traceback
-from inspect import currentframe as cf
 
+here = os.path.dirname(os.path.realpath(__file__))
 
 class PaddleHiQLatency(hiq.HiQSimple):
     def custom(s):
         s.o_paddle_run = hiq.mod('paddle.fluid.core_avx').PaddleInferPredictor.run
+
         @s.inserter
         def paddle_run(self) -> bool:
             return s.o_paddle_run(self)
+
         hiq.mod('paddle.fluid.core_avx').PaddleInferPredictor.run = paddle_run
 
     def custom_disable(s):
         hiq.mod('paddle.fluid.core_avx').PaddleInferPredictor.run = s.o_paddle_run
 
 
-# paddle.fluid.core_avx.PaddleInferPredictor.run
-driver = PaddleHiQLatency(
-    hiq_table_or_path=[
-        # ["paddle.fluid.core_avx", "PaddleInferPredictor", "run", "run"],
-        ["paddleocr", "", "draw_ocr", "draw_ocr"],
-        ["paddleocr", "PaddleOCR", "__init__", "init"],
-        ["paddleocr", "PaddleOCR", "ocr", "ocr"],
-        ["paddleocr", "PaddleOCR", "__call__", "txt_model"],
-        ["tools.infer.predict_det", "TextDetector", "__call__", "det_model"],
-        ["tools.infer.predict_rec", "TextRecognizer", "__call__", "rec_model"],
-        ["tools.infer.predict_cls", "TextClassifier", "__call__", "cls_model"],
-        ["tools.infer.utility", "", "create_predictor", "create_predictor"],
-        ["main", "", "main", "main"],
-        ["PIL.Image", "", "open", "pil_open"],
-        ["PIL.Image", "Image", "save", "pil_save"],
-    ]
-)
+driver = PaddleHiQLatency(f"{here}/paddleocr.conf")
 hiq.mod("main").main()
 driver.show()
