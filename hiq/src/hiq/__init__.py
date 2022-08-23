66904ebe86f66f863c4f19b1b9e57e705fd5889a
Fuheng Wu <fuheng.wu@oracle.com>
2022-08-23 16:26:16 -0700

    upgrade dependency version

--- hiq/src/hiq/__init__.py
+++ hiq/src/hiq/__init__.py
@@ -5,9 +5,9 @@
 #
 
 __author__ = "Fuheng Wu<fuheng.wu@oralce.com>"
-__date__ = "2022-04-03"
+__date__ = "2022-08-23"
 __doc__ = "HiQ is a declarative, non-intrusive, dynamic and transparent tracking and optimization system"
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __credits__ = "Henry Wu, Ivan Davchev, Jun Qian"
 
 import sys
