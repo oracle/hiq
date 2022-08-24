975ea663837f7537ce54802969a4cf2a652ed56f
HenryFuhengWu <wufuheng@gmail.com>
2022-08-23 19:12:13 -0700

    no cibuildwheel for pure python package v7

--- hiq/src/hiq/__init__.py
+++ hiq/src/hiq/__init__.py
@@ -7,7 +7,7 @@
 __author__ = "Fuheng Wu<fuheng.wu@oralce.com>"
 __date__ = "2022-08-23"
 __doc__ = "HiQ is a declarative, non-intrusive, dynamic and transparent tracking and optimization system"
-__version__ = "1.0.3"
+__version__ = "1.0.3test"
 __credits__ = "Henry Wu, Ivan Davchev, Jun Qian"
 
 import sys
