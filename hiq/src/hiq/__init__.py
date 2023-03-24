9c5bb0ef9cbd5c0590f29caaa49837fbcbe88d8f
Fuheng Wu <fuheng.wu@oracle.com>
2023-03-23 20:46:46 -0700

    remove *nix spyc in wheel

--- hiq/src/hiq/__init__.py
+++ hiq/src/hiq/__init__.py
@@ -5,9 +5,9 @@
 #
 
 __author__ = "Fuheng Wu<fuheng.wu@oralce.com>"
-__date__ = "2023-02-01"
+__date__ = "2023-03-23"
 __doc__ = "HiQ is a declarative, non-intrusive, dynamic and transparent tracking and optimization system"
-__version__ = "1.1.9"
+__version__ = "1.1.10"
 __credits__ = "Henry Wu, Ivan Davchev, Jun Qian"
 
 import sys
